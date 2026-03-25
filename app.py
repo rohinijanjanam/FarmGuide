feb067d09b55ceb8baa1c9dcf18a8f73f8663837
rohinijanjanam <rohinijanjanam1908@gmail.com>
2026-03-25 12:33:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -7,65 +6,27 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        print("LOGIN CLICKED")   # 👈 DEBUG LINE
-        return redirect('/home')  # 👈 FORCE REDIRECT
+        print("LOGIN CLICKED")
+        return redirect('/home')
 
     return render_template('login.html', show_navbar=False)
+
 # HOME PAGE
 @app.route('/home')
 def home():
     return render_template('index.html', show_navbar=True)
 
-
 @app.route('/scan')
 def scan():
     return render_template('scan.html', show_navbar=True)
-
 
 @app.route('/price')
 def price():
     return render_template('price.html', show_navbar=True)
 
-
 @app.route('/advisory')
 def advisory():
     return render_template('advisory.html', show_navbar=True)
 
-
 if __name__ == '__main__':
-    if __name__ == '__main__':
     app.run(debug=True)
-
-app = Flask(__name__)
-
-# LOGIN PAGE
-@app.route('/', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        print("LOGIN CLICKED")   # 👈 DEBUG LINE
-        return redirect('/home')  # 👈 FORCE REDIRECT
-
-    return render_template('login.html', show_navbar=False)
-# HOME PAGE
-@app.route('/home')
-def home():
-    return render_template('index.html', show_navbar=True)
-
-
-@app.route('/scan')
-def scan():
-    return render_template('scan.html', show_navbar=True)
-
-
-@app.route('/price')
-def price():
-    return render_template('price.html', show_navbar=True)
-
-
-@app.route('/advisory')
-def advisory():
-    return render_template('advisory.html', show_navbar=True)
-
-
-if __name__ == '__main__':
-	    app.run(debug=True)
